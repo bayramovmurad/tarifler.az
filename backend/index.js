c04a48bbbfa97f2e047b6ab4c1943609226b0b61
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-07-07 13:57:44 +0400

    setup(backend): connect server and database

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    })
+})
+.catch(error => {
+    console.log(error.message);
+})
+
